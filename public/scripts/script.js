6924dd9740085eb6665acf9ece2a4b6bba103d33
ambersr <amber.schalker@hva.nl>
2025-03-10 16:13:57 +0100

    Filterings JS voor linkjes en active state toevoegen #5

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,20 @@
-// 
+// Filter functie op categorie
 document.querySelectorAll("input[name='category']").forEach(radio => {
     radio.addEventListener("change", function () {
-      const selectedCategory = this.value;
-      const url = selectedCategory ? `/?category=${encodeURIComponent(selectedCategory)}` : "/";
-      window.location.href = url; // Pagina herladen met filter
-    });
+     const selectedCategory = this.value;
+
+    // Verwijder de 'active' class van alle labels en voeg deze toe aan de geselecteerde
+    document.querySelectorAll("input[name='category']").forEach(r => r.parentElement.classList.remove("active"));
+    this.parentElement.classList.add("active");
+
+    // URL aanpassen
+    window.location.href = selectedCategory ? `/webinars/?category=${encodeURIComponent(selectedCategory)}` : "/webinars";
   });
+  });
+
+// Herstel 'active' class bij het laden van de pagina
+window.addEventListener("load", () => {
+  const selectedCategory = new URLSearchParams(window.location.search).get('category') || '';
+  const selectedRadio = document.querySelector(`input[name='category'][value='${selectedCategory}']`);
+  if (selectedRadio) selectedRadio.parentElement.classList.add("active");
+});
